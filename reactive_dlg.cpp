efd869ada2ca13be851b9a9712cc1f6fc9b6c39f
sigma-axis <sigma-axis-51492481@hotmail.com>
2024-03-08 00:05:24 +0900

    v1.20

--- reactive_dlg.cpp
+++ reactive_dlg.cpp
@@ -1390,7 +1390,7 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD fdwReason, LPVOID lpvReserved)
 // 看板．
 ////////////////////////////////
 #define PLUGIN_NAME		"Reactive Dialog"
-#define PLUGIN_VERSION	"v1.20-pre1"
+#define PLUGIN_VERSION	"v1.20"
 #define PLUGIN_AUTHOR	"sigma-axis"
 #define PLUGIN_INFO_FMT(name, ver, author)	(name##" "##ver##" by "##author)
 #define PLUGIN_INFO		PLUGIN_INFO_FMT(PLUGIN_NAME, PLUGIN_VERSION, PLUGIN_AUTHOR)
